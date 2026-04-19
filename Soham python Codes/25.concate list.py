a59ec1fe060d57f7b8adfda8a2ac04170ccef77c
ks-155 <158799414+ks-155@users.noreply.github.com>
2026-04-19 17:48:03 +0530

    Update 25.concate list.py

--- Soham python Codes/25.concate list.py
+++ Soham python Codes/25.concate list.py
@@ -1,8 +1,7 @@
 lst1=[1,2,3,4,5]
 lst2=[6,7,8,9,10]
-print("1 is here :",lst1)
-print("2 is here :",lst2)
-def con(list1,list2):
-    return list1+list2
+def con(x,y):
+    return x+y
+    
 result=con(lst1,lst2)
 print("result is here :",result)
